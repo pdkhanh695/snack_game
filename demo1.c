3dd74a289bfad293e9c9e688bb20292e693e655e
pdkhanh <pdkhanh95@gmail.com>
2018-12-27 23:43:48 +0700

    First Commit

---
+++ demo1.c
@@ -0,0 +1,14 @@
+#include<iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main(void){
+int a = 0;
+int b = 0;
+int c  = a + b;
+cout << "Con mm may gia tri cua c la "<<endl;
+cout << c<<endl;
+
+return 0;
+}
---
+++ demo2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(void){
+int a = 5; 
+int b = 5;
+int c = a + b;
+printlf("xxx");
+
+return 0;
+}
